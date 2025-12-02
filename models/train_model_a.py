6eaa192363d108bcb518cc67df99b60b8f98e2c7
ahmedbasemdev <ahmedbasem.dev@gmail.com>
2025-12-02 15:46:11 +0200

    First Commit

---
+++ models/train_model_a.py
@@ -0,0 +1,13 @@
+# train_model_a.py
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+iris = load_iris()
+X, y = iris.data, iris.target
+
+model = LogisticRegression(max_iter=200)
+model.fit(X, y)
+
+with open("model_a.pkl", "wb") as f:
+    pickle.dump(model, f)
---
+++ models/train_model_b.py
@@ -0,0 +1,13 @@
+# train_model_b.py
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+iris = load_iris()
+X, y = iris.data, iris.target
+
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X, y)
+
+with open("model_b.pkl", "wb") as f:
+    pickle.dump(model, f)
